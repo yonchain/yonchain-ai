a2af3669316c2e745b358a98b37225b2ff325a83
yonchain-ai <15962850+yonchain-ai@user.noreply.gitee.com>
2025-08-03 21:12:53 +0800

    Initial commit

---
+++ src/main/java/com/dify4j/yonchainai/YonchainAiApplication.java
@@ -0,0 +1,13 @@
+package com.dify4j.yonchainai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YonchainAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YonchainAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dify4j/yonchainai/YonchainAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dify4j.yonchainai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YonchainAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
